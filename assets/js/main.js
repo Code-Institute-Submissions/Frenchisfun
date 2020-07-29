d4ab21b606de0c227dd9ff5d3c274a7a704f1789
Adrian Looney <adrian.looney@gmail.com>
2020-07-29 13:48:07 +0000

    multiple changes see read me

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,35 @@
+var words = {
+  Dog: "Chien",
+  Bike: "Vélo",
+  Guitar: "Guitare",
+  Book: "Livre",
+};
 $(document).ready(function () {
+  // If the user clicks on a english word then an french word run this function
   $(".frbtn").click(function () {
-    console.log(this.value);
+    $(".frbtn").removeClass("btn-secondary").addClass("btn-primary"); // Change button color to show selection
+    $(this).removeClass("btn-primary").addClass("btn-secondary"); // Restore to org color if user selects another button
+    $("#frenchText").val(this.value); // Display selected word in textarea
+    console.log(words[$("#frenchText").val()]); // temp log
+    if (words[$("#englishText").val()] === this.value) {
+      $(this).addClass("removeTest"); // Add CSS class to remove button on successful match.
+      let englishWord = $("#englishText").val();
+      document.getElementById(englishWord).classList.add("removeTest");
+    }
+  });
+
+  // If the user clicks on a french word then an english word run this function
+  $(".engbtn").click(function () {
+    $(".engbtn").removeClass("btn-secondary").addClass("btn-primary");
     $(this).removeClass("btn-primary").addClass("btn-secondary");
-    $("#frenchText").val(this.value);
+    $("#englishText").val(this.value);
+    console.log(words[$("#englishText").val()]); // temp log
+    if (words[this.value] === $("#frenchText").val()) {
+      $(this).addClass("removeTest");
+      let frenchWord = $("#frenchText").val();
+      document.getElementById(frenchWord).classList.add("removeTest");
+    }
+
+    // if (words[this.value] === $("#frenchText").val()) {
   });
 });
